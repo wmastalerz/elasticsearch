34299d6999fcdceb0933c457d52dccd559ac1a45
wmastalerz <wmastalerz@tlen.pl>
2023-04-16 20:17:51 +0200

    remove duble files

--- metallb.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-kubectl apply -f metallb-native.yaml
-kubectl wait --namespace metallb-system \
-                --for=condition=ready pod \
-                --selector=app=metallb \
-                --timeout=90s
-ippool=$(docker network inspect -f '{{.IPAM.Config}}' kind |awk {'print substr($2,1,7)'})
-
