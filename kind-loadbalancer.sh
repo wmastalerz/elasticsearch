b15e9d3c165210916f798c5430e27839fd9aad0e
wmastalerz <wmastalerz@tlen.pl>
2023-04-21 08:43:30 +0200

    fix load balancer ip issue

--- kind-loadbalancer.sh
+++ kind-loadbalancer.sh
@@ -7,7 +7,6 @@ kubectl wait --namespace metallb-system \
 
 ippool=$(docker network inspect -f '{{.IPAM.Config}}' kind)
 ipprefix="${ippool:2:7}"
-kubectl apply -f metallb-config.yaml
 metalb=$(cat metallb-config.yaml)
 echo "${metalb//172.19./$ipprefix}" | kubectl apply -f -
 
